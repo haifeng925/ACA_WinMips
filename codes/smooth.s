bb4b19f4a046d42ee55d253f30e09e9478de9a4e
Haifeng <haifeng925@gmail.com>
2013-06-10 04:46:50 +0200

    coding

--- codes/smooth.s
+++ codes/smooth.s
@@ -56,13 +56,79 @@ last_norm1:
         c.lt.d  f3, f0
         bc1t  else_last_norm1
         add.d f1, f1, f3
-        j     result 
+        j     cal_result 
 
 else_last_norm1:
         sub.d f3, f0, f3
         add.d f1, f1, f3
 
-result:
+cal_result:
+        daddu r3, r0, r0            ; i=0
+        l.d   f3, sample(r3)
+        sdc1  f3, result(r3)        ; i=0, result[0] = sample[0]
+        daddi r3, r3, 1             ; r3 = 1, i=1
+        dsll  r6, r3, 3
+
+        daddi r5, r0, 1             ; j = 1;
+        mtc1  r0, f4                ; f4 for temp result
+
+for_within_i_1:
+        stl   r7, r5, r4
+        beqz  r7, calculate_with_norm2
+        dsub  r8, r5, r3            
+        dsll  r8, r8, 3             ;&sample[i-2+j] sample[j-1]
+        dsll  r9, r5, 3             ;&coeff[j]
+        l.d   f5, sample(r8)
+        l.d   f6, coeff(r9)
+        mult.d f7, f5, f6
+        add.d f4, f4, f7
+        daddi r5, r5, 1
+        j     for_within_i_1
+calculate_with_norm2:
+        div.d f4, f4, f2            ; result[1]/=norm2
+        sdc1  f4, result(r6)
+        daddi r3, r3, 1             ; r3 = 2 = i
+
+        ;; for n: 2 -> n-2
+
+        dsub r6, r1, r3             ; r6 = N_SAMPLES - 2
+normal_result:
+        slt  r7, r3, r6             ; r3=i
+        beqz r7, last_two_result
+
+        mtc1 r0, f4                 ; f4 for temp result
+        daddi r5, r0, r0            ; r5 = j = 0
+for_within_normal:
+        stl  r7, r5, r2             ; r2 = N_COEFFS
+        beqz calcu_with_norm1
+        daddi r8, r0, 2
+        dsub  r9, r3, r8
+        daddu r9, r9, r5            ; r9 = i-2+j
+        dsll  r9, r9, 3
+        dsll  r10, r5, 3
+        l.d   f5, sample(r9)
+        l.d   f6, coeff(r10)
+        mult.d f7, f5, f6
+        add.d  f4, f4, f7
+        daddi r5, r5, 1
+        j     for_within_normal
+calcu_with_norm1:
+        div.d f4, f4, f1
+
+        daddi r3, r3, 1
+        j     normal_result
+last_two_result:
+        mtc1 r0, f4
+        daddi r5, r0, r0
+        
+
+;for1: 
+ ;       slt   r5, r3, r1            ; r1= N_SAMPLES, if i<N_SAMPLES
+  ;      beqz  r5, end_for1
+
+
+end_for1:
+        
 
 printresult:
     halt
